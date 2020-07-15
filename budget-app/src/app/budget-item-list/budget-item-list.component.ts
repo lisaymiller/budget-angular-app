0a0c14b045415e5fc3f4c0e0c7a153dab127195e
Lisa Miller <lisayevonne@outlook.com>
2020-07-15 13:33:01 -0400

    budget items dynamic and deletable

--- budget-app/src/app/budget-item-list/budget-item-list.component.ts
+++ budget-app/src/app/budget-item-list/budget-item-list.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
 
 @Component({
   selector: 'app-budget-item-list',
   templateUrl: './budget-item-list.component.html',
-  styleUrls: ['./budget-item-list.component.scss']
+  styleUrls: ['./budget-item-list.component.scss'],
 })
 export class BudgetItemListComponent implements OnInit {
+  @Input() budgetItems: BudgetItem[];
+  @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onDeleteButtonClicked(item: BudgetItem) {
+    this.delete.emit(item);
   }
-
 }
